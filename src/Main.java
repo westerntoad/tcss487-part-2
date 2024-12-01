de1611b7595bbd2bdb86e5914a10dd4a33947127
christian <christianbonnalie@gmail.com>
2024-11-30 20:09:00 -0800

    signature generation and verification implemented

--- src/Main.java
+++ src/Main.java
@@ -7,31 +7,39 @@ import java.security.SecureRandom;
 import java.util.*;
 
 /**
- *
  * @author Christian Bonnalie
  * @author Abraham Engebretson
  * @author Ethan Somdahl
  */
 public class Main {
 
-    public static final HexFormat HEXF = HexFormat.of();
+    private static final HexFormat HEXF = HexFormat.of();
 
-    private static BigInteger generateKeyPair(byte[] passphrase, String outputDir) {
+    private static final Map<String, String> DEFAULT_PATHS = new HashMap<>();
 
-        if (outputDir == null) {
-            outputDir = "../public-key.txt";
-        }
+    static {
+        DEFAULT_PATHS.put("keygen", "../public-key.txt");
+        DEFAULT_PATHS.put("encrypt", "../encrypted-message.txt");
+        DEFAULT_PATHS.put("decrypt", "../decrypted-message.txt");
+        DEFAULT_PATHS.put("generate", "../signature.txt");
+    }
 
+    private static BigInteger generatePrivateKey(byte[] passphrase) {
         // init SHAKE-128, absorb passphrase
         SHA3SHAKE sponge = new SHA3SHAKE();
         sponge.init(128);
         sponge.absorb(passphrase);
-
         // squeeze a 256-bit byte array
         byte[] output = sponge.squeeze(32);
-
         // create a BigInteger from it, reduce this value mod r.
-        BigInteger s = new BigInteger(output).mod(Edwards.getR());
+        return new BigInteger(output).mod(Edwards.getR());
+    }
+
+    private static BigInteger generateKeyPair(byte[] passphrase, String outputDir) {
+
+
+        // generate s from a passphrase
+        BigInteger s = generatePrivateKey(passphrase);
         // compute V <- sG
         Edwards instance = new Edwards();
         Edwards.Point V = instance.gen().mul(s);
@@ -57,7 +65,6 @@ public class Main {
     }
 
     private static void encrypt(String publicKeyFile, String message, String outputDir) {
-
         try {
             byte[] messageBytes = Files.readAllBytes(Paths.get(message));
 
@@ -73,7 +80,7 @@ public class Main {
 
             // create point V from public key
             Edwards instance = new Edwards();
-            Edwards.Point V = instance.getPoint(Vy, false);
+            Edwards.Point V = instance.getPoint(Vy, Vy.testBit(0));
 
             // generate random k mod r
             int rBytes = (Edwards.getR().bitLength() + 7) >> 3;
@@ -130,9 +137,8 @@ public class Main {
     }
 
     private static void decrypt(String passphrase, String inputDir, String outputDir) {
-
         // recompute the private key s
-        BigInteger s = generateKeyPair(passphrase.getBytes(), null);
+        BigInteger s = generatePrivateKey(passphrase.getBytes());
 
         try {
             // read input from file
@@ -187,6 +193,78 @@ public class Main {
         }
     }
 
+    private static void generateSignature(String filePath, String passphrase, String outputDir) {
+        // recompute s from the passphrase
+        BigInteger s = generatePrivateKey(passphrase.getBytes());
+
+        // generate random k mod r
+        int rBytes = (Edwards.getR().bitLength() + 7) >> 3;
+        var k = new BigInteger(new SecureRandom().generateSeed(rBytes << 1)).mod(Edwards.getR());
+
+        // compute U = kG
+        Edwards instance = new Edwards();
+        Edwards.Point U = instance.gen().mul(k);
+        // init sha256, absorb Uy and message
+        SHA3SHAKE sha256 = new SHA3SHAKE();
+        sha256.init(256);
+        sha256.absorb(U.y.toByteArray());
+        try {
+            sha256.absorb(Files.readAllBytes(Paths.get(filePath)));
+        } catch (Exception e) {
+            System.out.println("Error: Invalid path to file. Please try again.");
+        }
+        // extract the 256-bit byte array digest
+        byte[] digest = sha256.digest();
+        // convert to BI and reduce it mod r
+        BigInteger h = new BigInteger(digest).mod(Edwards.getR());
+        // compute z = (k-h*s) mod r
+        BigInteger z = k.subtract(h.multiply(s)).mod(Edwards.getR());
+
+        // the signature is the pair (h,z)
+        try (FileOutputStream fos = new FileOutputStream(outputDir)) {
+            fos.write(HEXF.formatHex(h.toByteArray()).getBytes());
+            fos.write("\n".getBytes());
+            fos.write(HEXF.formatHex(z.toByteArray()).getBytes());
+        } catch (Exception e) {
+            System.out.println("Error: Invalid path to file. Please try again.");
+        }
+    }
+
+    private static void verifySignature(String messageFile, String signatureFile, String publicKeyFile) {
+
+        try {
+            List<String> signatureLines = Files.readAllLines(Paths.get(signatureFile));
+            List<String> publicKeyLines = Files.readAllLines(Paths.get(publicKeyFile));
+            byte[] message = Files.readAllBytes(Paths.get(messageFile));
+
+            BigInteger h = new BigInteger(HEXF.parseHex(signatureLines.get(0)));
+            BigInteger z = new BigInteger(HEXF.parseHex(signatureLines.get(1)));
+
+            BigInteger Vy = new BigInteger(HEXF.parseHex(publicKeyLines.get(1)));
+            Edwards instance = new Edwards();
+            Edwards.Point V = instance.getPoint(Vy, Vy.testBit(0));
+
+            Edwards.Point uPrime = instance.gen().mul(z).add(V.mul(h));
+
+            SHA3SHAKE sha256 = new SHA3SHAKE();
+            sha256.init(256);
+            sha256.absorb(uPrime.y.toByteArray());
+            sha256.absorb(message);
+
+            byte[] digest = sha256.digest();
+            BigInteger hPrime = new BigInteger(digest).mod(Edwards.getR());
+
+            if (h.equals(hPrime)) {
+                System.out.println("Signature Verified!");
+            } else {
+                System.out.println("Signature not verified.");
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error: Invalid path to file. Please try again.");
+        }
+    }
+
     public static void main(String[] args) {
 
         switch (args[0].toLowerCase()) {
@@ -194,7 +272,7 @@ public class Main {
                 if (args.length == 2) {
                     // 0 = "keygen"
                     // 1 = passphrase
-                    generateKeyPair(args[1].getBytes(), null);
+                    generateKeyPair(args[1].getBytes(), DEFAULT_PATHS.get("keygen"));
                 } else if (args.length == 3) {
                     // 0 = "keygen"
                     // 1 = passphrase
@@ -209,7 +287,7 @@ public class Main {
                     // 0 = "encrypt"
                     // 1 = public key file
                     // 2 = message
-                    encrypt(args[1], args[2], "../encrypted-message.txt");
+                    encrypt(args[1], args[2], DEFAULT_PATHS.get("encrypt"));
                 } else if (args.length == 4) {
                     // 0 = "encrypt"
                     // 1 = public key file
@@ -225,7 +303,7 @@ public class Main {
                     // 0 = "decrypt"
                     // 1 = passphrase
                     // 2 = input file
-                    decrypt(args[1], args[2], "../decrypted-message.txt");
+                    decrypt(args[1], args[2], DEFAULT_PATHS.get("decrypt"));
                 } else if (args.length == 4) {
                     // 0 = "decrypt"
                     // 1 = passphrase
@@ -236,6 +314,32 @@ public class Main {
                     System.out.println("Error: Invalid number of arguments.");
                 }
                 break;
+            case "generate":
+                if (args.length == 3) {
+                    // 0 = "generate"
+                    // 1 = file path
+                    // 2 = passphrase
+                    generateSignature(args[1], args[2], DEFAULT_PATHS.get("generate"));
+                } else if (args.length == 4) {
+                    // 0 = "generate"
+                    // 1 = file path
+                    // 2 = passphrase
+                    // 3 = output file
+                    generateSignature(args[1], args[2], args[3]);
+                } else {
+                    System.out.println("Error: Invalid number of arguments.");
+                }
+                break;
+            case "verify":
+                if (args.length == 4) {
+                    // 0 = "verify"
+                    // 1 = message file
+                    // 2 = signature file
+                    // 3 = public key file
+                    verifySignature(args[1], args[2], args[3]);
+                } else {
+                    System.out.println("Error: Invalid number of arguments.");
+                }
             default:
                 System.out.println("Error: First argument not a valid application feature.");
                 break;
